f2806794496eabe097f2845c428b13026dec54be
satyasidd18 <satyasidd18@gmail.com>
2025-08-10 00:33:08 -0400

    Update RecruiterProfileResponse.java

--- pamtenproject-master/src/main/java/com/careermatch/pamtenproject/dto/RecruiterProfileResponse.java
+++ pamtenproject-master/src/main/java/com/careermatch/pamtenproject/dto/RecruiterProfileResponse.java
@@ -1,17 +1,10 @@
-package com.careermatch.pamtenproject.dto;
-
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
 @Builder
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class RecruiterProfileResponse {
     private String userId;
-    private String name;
+    private String fullName;       // ✅ Add this
     private String email;
     private String companyName;
     private String phoneNumber;
