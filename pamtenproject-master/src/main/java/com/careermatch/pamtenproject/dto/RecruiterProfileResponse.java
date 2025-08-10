3a2fe63065959f920340a2cea9b576758ddbc258
satyasidd18 <satyasidd18@gmail.com>
2025-08-10 00:36:38 -0400

    Update RecruiterProfileResponse.java

--- pamtenproject-master/src/main/java/com/careermatch/pamtenproject/dto/RecruiterProfileResponse.java
+++ pamtenproject-master/src/main/java/com/careermatch/pamtenproject/dto/RecruiterProfileResponse.java
@@ -1,3 +1,14 @@
+package com.careermatch.pamtenproject.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 @Data
 @NoArgsConstructor
