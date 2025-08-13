70baa07e624744bb205903195529e4c2e7c26aac
satyasidd18 <satyasidd18@gmail.com>
2025-08-13 11:22:03 -0400

    Update RecruiterProfileResponse.java

--- pamtenproject-master/src/main/java/com/careermatch/pamtenproject/dto/RecruiterProfileResponse.java
+++ pamtenproject-master/src/main/java/com/careermatch/pamtenproject/dto/RecruiterProfileResponse.java
@@ -1,9 +1,12 @@
 package com.careermatch.pamtenproject.dto;
 
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
 
 @Data
 @Builder
@@ -13,9 +16,24 @@ public class RecruiterProfileResponse {
     private String userId;
     private String fullName;
     private String email;
-    private String companyName;
-    private String phoneNumber;
-    private String jobTitle;
     private String roleName;
-    private Boolean profileCompleted;
+    private boolean profileCompleted;
+
+    // Employer details
+    private String employerNumber;
+    private String organizationName;
+    private String hrName;
+    private String hrEmail;
+    private String endClient;
+    private String vendorName;
+
+    // Personal details
+    private LocalDate dateOfBirth;
+    private String gender;
+
+    // Industry
+    private String industryName;
+    private String industryDescription;
+
+    private String message;
 }
